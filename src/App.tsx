821ecd04a4f39207eed716cb2f2148960534ebf6
ckhe1215 <ckhe1215@gmail.com>
2026-01-29 10:07:58 +0900

    chore: 프로바이더 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import { ErrorBoundary, Suspense } from '@suspensive/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router';
 import ErrorSection from './components/ErrorSection';
-import { CurrencyProvider } from './providers/CurrencyProvider';
 import GlobalProvider from './providers/GlobalProvider';
 import router from './router';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <GlobalProvider>
-        <ErrorBoundary fallback={<ErrorSection />}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <CurrencyProvider>
-              <RouterProvider router={router} />
-            </CurrencyProvider>
-          </Suspense>
-        </ErrorBoundary>
-      </GlobalProvider>
-    </QueryClientProvider>
+    <GlobalProvider>
+      <ErrorBoundary fallback={<ErrorSection />}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </ErrorBoundary>
+    </GlobalProvider>
   );
 }
 
--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -1,7 +1,17 @@
 import { EnhancedToastProvider } from '@/ui-lib/components/toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CurrencyProvider } from './CurrencyProvider';
+
+const queryClient = new QueryClient();
 
 const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
-  return <EnhancedToastProvider>{children}</EnhancedToastProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <EnhancedToastProvider>
+        <CurrencyProvider>{children}</CurrencyProvider>
+      </EnhancedToastProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default GlobalProvider;
