9f1b5ceacd872032873cff87fad427986df80634
ckhe1215 <ckhe1215@gmail.com>
2026-01-30 11:21:50 +0900

    chore: error boundary 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { ErrorBoundary, Suspense } from '@suspensive/react';
 import { RouterProvider } from 'react-router';
-import ErrorSection from './components/ErrorSection';
 import GlobalProvider from './providers/GlobalProvider';
 import router from './router';
 
 function App() {
   return (
     <GlobalProvider>
-      <ErrorBoundary fallback={({ reset }) => <ErrorSection onRetry={reset} />}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <RouterProvider router={router} />
-        </Suspense>
-      </ErrorBoundary>
+      <RouterProvider router={router} />
     </GlobalProvider>
   );
 }
--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -1,4 +1,6 @@
+import ErrorSection from '@/components/ErrorSection';
 import { EnhancedToastProvider } from '@/ui-lib/components/toast';
+import { ErrorBoundary, Suspense } from '@suspensive/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { CartProvider } from './CartProvider';
 import { CurrencyProvider } from './CurrencyProvider';
@@ -8,11 +10,15 @@ const queryClient = new QueryClient();
 const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <EnhancedToastProvider>
-        <CurrencyProvider>
-          <CartProvider>{children}</CartProvider>
-        </CurrencyProvider>
-      </EnhancedToastProvider>
+      <ErrorBoundary fallback={({ reset }) => <ErrorSection onRetry={reset} />}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <EnhancedToastProvider>
+            <CurrencyProvider>
+              <CartProvider>{children}</CartProvider>
+            </CurrencyProvider>
+          </EnhancedToastProvider>
+        </Suspense>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 };
