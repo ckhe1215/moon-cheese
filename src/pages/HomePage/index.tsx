f4a7eeac2145fd0ebeca1a15c0f6fd4c3ba2726c
ckhe1215 <ckhe1215@gmail.com>
2026-02-05 20:32:29 +0900

    refactor: 최근 구매한 상품

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,19 @@
-import { productListQueryOptions, recentProductListQueryOptions, type Product } from '@/api/queryOptions';
+import {
+  productListQueryOptions,
+  recentProductListQueryOptions,
+  type Product,
+  type RecentProduct,
+} from '@/api/queryOptions';
 import AsyncBoundary from '@/components/AsyncBoundary';
-import { Text } from '@/ui-lib';
+import { Spacing, Text } from '@/ui-lib';
 import { SuspenseQuery } from '@suspensive/react-query';
-import { Box, Grid, styled } from 'styled-system/jsx';
+import { groupBy } from 'es-toolkit';
+import { Box, Flex, Grid, styled } from 'styled-system/jsx';
 import BannerSection from './components/BannerSection';
 import CategorySelector, { type TabType } from './components/CategorySelector';
 import CurrentLevelSection from './components/CurrentLevelSection';
 import GetPointInfo from './components/GetPointInfo';
-import RecentPurchasedProductList from './components/RecentPurchasedProductList';
+import RecentPurchasedProductItem from './components/RecentPurchasedProductItem';
 import { CheeseItem, CrackerItem, TeaItem } from './components/SellingProductItem';
 
 function HomePage() {
@@ -27,11 +33,21 @@ function HomePage() {
         </GetPointInfo>
       </AsyncBoundary>
 
-      <AsyncBoundary>
-        <SuspenseQuery {...recentProductListQueryOptions()}>
-          {({ data }) => <RecentPurchasedProductList items={data.recentProducts} />}
-        </SuspenseQuery>
-      </AsyncBoundary>
+      <styled.section css={{ px: 5, pt: 4, pb: 8 }}>
+        <Text variant="H1_Bold">최근 구매한 상품</Text>
+        <Spacing size={4} />
+        <Flex css={BACKGROUND_STYLE} direction={'column'}>
+          <AsyncBoundary>
+            <SuspenseQuery {...recentProductListQueryOptions()}>
+              {({ data }) =>
+                reducePriceById(data.recentProducts).map((product: RecentProduct) => (
+                  <RecentPurchasedProductItem key={product.id} product={product} />
+                ))
+              }
+            </SuspenseQuery>
+          </AsyncBoundary>
+        </Flex>
+      </styled.section>
 
       <AsyncBoundary>
         <SuspenseQuery {...productListQueryOptions()}>
@@ -80,3 +96,19 @@ export default HomePage;
 
 const matchesCategory = (category: Product['category'], currentTab: TabType) =>
   currentTab === 'ALL' || category === currentTab;
+
+const reducePriceById = (recentProducts: RecentProduct[]) => {
+  const productsById = Object.values(groupBy(recentProducts, product => product.id));
+  return productsById.map(products => ({
+    ...products[0],
+    price: products.reduce((acc, item) => acc + item.price, 0),
+  }));
+};
+
+const BACKGROUND_STYLE = {
+  bg: 'background.01_white',
+  px: 5,
+  py: 4,
+  gap: 4,
+  rounded: '2xl',
+};
