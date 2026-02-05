632a0a7f6d9d3a2293a6b3bc52270b9d1cb1420d
ckhe1215 <ckhe1215@gmail.com>
2026-02-06 07:18:53 +0900

    refactor: global provider async boundary구현

--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -1,6 +1,5 @@
-import ErrorSection from '@/components/ErrorSection';
+import AsyncBoundary from '@/components/AsyncBoundary';
 import { EnhancedToastProvider } from '@/ui-lib/components/toast';
-import { ErrorBoundary, Suspense } from '@suspensive/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { CartProvider } from './CartProvider';
 import { CurrencyProvider } from './CurrencyProvider';
@@ -10,15 +9,13 @@ const queryClient = new QueryClient();
 const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ErrorBoundary fallback={({ reset }) => <ErrorSection onRetry={reset} />}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <EnhancedToastProvider>
-            <CurrencyProvider>
-              <CartProvider>{children}</CartProvider>
-            </CurrencyProvider>
-          </EnhancedToastProvider>
-        </Suspense>
-      </ErrorBoundary>
+      <AsyncBoundary>
+        <EnhancedToastProvider>
+          <CurrencyProvider>
+            <CartProvider>{children}</CartProvider>
+          </CurrencyProvider>
+        </EnhancedToastProvider>
+      </AsyncBoundary>
     </QueryClientProvider>
   );
 };
